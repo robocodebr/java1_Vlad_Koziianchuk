c83c1fde5d53c85053e5ad030891354b9fd251c4
robocodebr <robocodebr@gmail.com>
2020-09-13 16:04:41 +0300

    first commit

---
+++ lesson2/src/com/company/Main.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+//        int d = 20;
+//        int m = 9;
+//        int y =1;
+//        if (d > m){
+//            System.out.println(d);
+//        } else if (y<m) {
+//        System.out.println(m);
+//        } else {System.out.println(y);}
+//        }
+//
+//
+//            String f = "fk";
+//            String k = "fkgg";
+//            String fr = "df";
+//            System.out.println(f.length());
+//            System.out.println(k.length());
+//            System.out.println(fr.length());
+
+//            String f = "Vlad";
+//        String l= "Koziianchuk";
+//        String m ="Oleksadrovich";
+//
+//        if (f.length() > l.length() && f.length()>m.length()  ){
+//            System.out.println(f);
+//        } else if (l.length()<m.length() && m.length()>f.length()) {
+//                System.out.println(m);
+//        }else {
+//                System.out.println(l);
+//        }
+
+//        String o = "0123456";
+//        String q = "1391215";
+//        String sd = "749864";
+//            System.out.println(o.length());
+//            System.out.println(q.length());
+//            System.out.println(sd.length());
+            Random random = new Random();
+            int i = random.nextInt();
+
+
+            int m = random.nextInt();
+
+            int p = random.nextInt();
+            if (p > m && p>i){
+          System.out.println(p);
+      } else if (i<m) {
+      System.out.println(m);
+      } else {System.out.println(i);}
+        }
+//                    String o = "0123456";
+//        String q = "1391215";
+//        String sd = "749864";
+//            o[0];
+//            o[1];
+//            o[3];
+//            o[4];
+//            o[5];
+//            o[6];
+
+
+
+
+
+    }
+
